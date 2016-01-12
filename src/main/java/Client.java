efdc8847fa99a9baf17e4cfee5874efca9d7aa33
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-01-12 16:55:03 +0200

    Refactor code, separate correction functionality from data retrieval.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,9 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import dao.LiveNode;
+import dao.LiveNodeDetails;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,43 +16,66 @@ public class Client {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<?> response = restTemplate.getForEntity(url, String.class);
         ObjectMapper mapper = new ObjectMapper();
-        correctJsonString(mapper, response.getBody().toString());
+        JsonNode rootNode = null;
+        try {
+            rootNode = mapper.readTree(response.getBody().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        correctJsonString(rootNode);
+        getDataNodes(rootNode);
+
     }
 
-    public static JsonNode correctJsonString(ObjectMapper mapper, String body) {
-        JsonNode rootNode = null;
+    public static JsonNode correctJsonString(JsonNode rootNode) {
+        List<String> nodeTypes = Arrays.asList("LiveNodes", "DeadNodes", "DecomNodes");
+        ObjectMapper mapper = new ObjectMapper();
+
+        for (String nodeType : nodeTypes) {
+            JsonNode liveNodes = rootNode.findPath(nodeType);
+            String lnString = liveNodes.toString();
+            lnString = lnString.replaceAll("\\\\", "");
+            lnString = lnString.substring(1, lnString.length() - 1);
+            JsonNode lNodes = null;
+            try {
+                lNodes = mapper.readTree(lnString);
+                ((ObjectNode) rootNode.findPath("beans").get(0)).replace(nodeType, lNodes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         try {
-            rootNode = mapper.readTree(body);
-
-            List<String> nodeTypes = Arrays.asList("LiveNodes", "DeadNodes", "DecomNodes");
-
-            for (String nodeType : nodeTypes) {
-                JsonNode liveNodes = rootNode.findPath(nodeType);
-                String lnString = liveNodes.toString();
-                lnString = lnString.replaceAll("\\\\", "");
-                lnString = lnString.substring(1, lnString.length() - 1);
-                JsonNode lNodes = mapper.readTree(lnString);
-                ((ObjectNode) rootNode.findPath("beans").get(0)).replace(nodeType, lNodes);
-            }
             System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(rootNode));
-
-
-            JsonNode nodes =  rootNode.findPath("LiveNodes");
-
-            Iterator<Map.Entry<String, JsonNode>> nodeIterator = nodes.fields();
-            while(nodeIterator.hasNext()) {
-                Map.Entry<String, JsonNode> jsonParam = nodeIterator.next();
-                String nodeName = jsonParam.getKey();
-                LiveNode liveNode = mapper.treeToValue(jsonParam.getValue(), LiveNode.class);
-                System.out.println(nodeName + " " + liveNode.getInfoAddr());
-            }
-            
-        } catch (IOException e1) {
-            e1.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
 
         return rootNode;
     }
+
+    public static List<LiveNode> getDataNodes(JsonNode rootNode) {
+        List<LiveNode> liveNodes = new ArrayList();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode nodes =  rootNode.findPath("LiveNodes");
+
+        Iterator<Map.Entry<String, JsonNode>> nodeIterator = nodes.fields();
+        while(nodeIterator.hasNext()) {
+            Map.Entry<String, JsonNode> jsonParam = nodeIterator.next();
+            String nodeName = jsonParam.getKey();
+            LiveNodeDetails liveNodeDetails = null;
+            try {
+                liveNodeDetails = mapper.treeToValue(jsonParam.getValue(), LiveNodeDetails.class);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            liveNodes.add(new LiveNode(nodeName, liveNodeDetails));
+            //System.out.println(nodeName + " " + liveNodeDetails.getInfoAddr());
+        }
+
+        return liveNodes;
+    }
+
 
 }
