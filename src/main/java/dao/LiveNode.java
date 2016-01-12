58411754745640ea30b8beb6f648226051b50788
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-01-12 16:54:08 +0200

    Update models in dto.

--- src/main/java/dao/LiveNode.java
+++ src/main/java/dao/LiveNode.java
@@ -1,148 +1,27 @@
 package dao;
 
 public class LiveNode {
-    private String infoAddr;
-    private String infoSecureAddr;
-    private String xferaddr;
-    private Long lastContact;
-    private Long usedSpace;
-    private String adminState;
-    private Long nonDfsUsedSpace;
-    private Long capacity;
-    private Long numBlocks;
-    private String version;
-    private Long used;
-    private Long remaining;
-    private Long blockScheduled;
-    private Long blockPoolUsed;
-    private Double blockPoolUsedPercent;
-    private Long volfails;
+    private String nodeName;
+    private LiveNodeDetails liveNodeDetails;
 
-    public String getInfoAddr() {
-        return infoAddr;
+    public LiveNode(String nodeName, LiveNodeDetails liveNodeDetails) {
+        this.nodeName = nodeName;
+        this.liveNodeDetails = liveNodeDetails;
     }
 
-    public void setInfoAddr(String infoAddr) {
-        this.infoAddr = infoAddr;
+    public String getNodeName() {
+        return nodeName;
     }
 
-    public String getInfoSecureAddr() {
-        return infoSecureAddr;
+    public void setNodeName(String nodeName) {
+        this.nodeName = nodeName;
     }
 
-    public void setInfoSecureAddr(String infoSecureAddr) {
-        this.infoSecureAddr = infoSecureAddr;
+    public LiveNodeDetails getLiveNodeDetails() {
+        return liveNodeDetails;
     }
 
-    public String getXferaddr() {
-        return xferaddr;
-    }
-
-    public void setXferaddr(String xferaddr) {
-        this.xferaddr = xferaddr;
-    }
-
-    public Long getLastContact() {
-        return lastContact;
-    }
-
-    public void setLastContact(Long lastContact) {
-        this.lastContact = lastContact;
-    }
-
-    public Long getUsedSpace() {
-        return usedSpace;
-    }
-
-    public void setUsedSpace(Long usedSpace) {
-        this.usedSpace = usedSpace;
-    }
-
-    public String getAdminState() {
-        return adminState;
-    }
-
-    public void setAdminState(String adminState) {
-        this.adminState = adminState;
-    }
-
-    public Long getNonDfsUsedSpace() {
-        return nonDfsUsedSpace;
-    }
-
-    public void setNonDfsUsedSpace(Long nonDfsUsedSpace) {
-        this.nonDfsUsedSpace = nonDfsUsedSpace;
-    }
-
-    public Long getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(Long capacity) {
-        this.capacity = capacity;
-    }
-
-    public Long getNumBlocks() {
-        return numBlocks;
-    }
-
-    public void setNumBlocks(Long numBlocks) {
-        this.numBlocks = numBlocks;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public Long getUsed() {
-        return used;
-    }
-
-    public void setUsed(Long used) {
-        this.used = used;
-    }
-
-    public Long getRemaining() {
-        return remaining;
-    }
-
-    public void setRemaining(Long remaining) {
-        this.remaining = remaining;
-    }
-
-    public Long getBlockScheduled() {
-        return blockScheduled;
-    }
-
-    public void setBlockScheduled(Long blockScheduled) {
-        this.blockScheduled = blockScheduled;
-    }
-
-    public Long getBlockPoolUsed() {
-        return blockPoolUsed;
-    }
-
-    public void setBlockPoolUsed(Long blockPoolUsed) {
-        this.blockPoolUsed = blockPoolUsed;
-    }
-
-    public Double getBlockPoolUsedPercent() {
-        return blockPoolUsedPercent;
-    }
-
-    public void setBlockPoolUsedPercent(Double blockPoolUsedPercent) {
-        this.blockPoolUsedPercent = blockPoolUsedPercent;
-    }
-
-    public Long getVolfails() {
-        return volfails;
-    }
-
-    public void setVolfails(Long volfails) {
-        this.volfails = volfails;
+    public void setLiveNodeDetails(LiveNodeDetails liveNodeDetails) {
+        this.liveNodeDetails = liveNodeDetails;
     }
 }
---
+++ src/main/java/dao/LiveNodeDetails.java
@@ -0,0 +1,148 @@
+package dao;
+
+public class LiveNodeDetails {
+    private String infoAddr;
+    private String infoSecureAddr;
+    private String xferaddr;
+    private Long lastContact;
+    private Long usedSpace;
+    private String adminState;
+    private Long nonDfsUsedSpace;
+    private Long capacity;
+    private Long numBlocks;
+    private String version;
+    private Long used;
+    private Long remaining;
+    private Long blockScheduled;
+    private Long blockPoolUsed;
+    private Double blockPoolUsedPercent;
+    private Long volfails;
+
+    public String getInfoAddr() {
+        return infoAddr;
+    }
+
+    public void setInfoAddr(String infoAddr) {
+        this.infoAddr = infoAddr;
+    }
+
+    public String getInfoSecureAddr() {
+        return infoSecureAddr;
+    }
+
+    public void setInfoSecureAddr(String infoSecureAddr) {
+        this.infoSecureAddr = infoSecureAddr;
+    }
+
+    public String getXferaddr() {
+        return xferaddr;
+    }
+
+    public void setXferaddr(String xferaddr) {
+        this.xferaddr = xferaddr;
+    }
+
+    public Long getLastContact() {
+        return lastContact;
+    }
+
+    public void setLastContact(Long lastContact) {
+        this.lastContact = lastContact;
+    }
+
+    public Long getUsedSpace() {
+        return usedSpace;
+    }
+
+    public void setUsedSpace(Long usedSpace) {
+        this.usedSpace = usedSpace;
+    }
+
+    public String getAdminState() {
+        return adminState;
+    }
+
+    public void setAdminState(String adminState) {
+        this.adminState = adminState;
+    }
+
+    public Long getNonDfsUsedSpace() {
+        return nonDfsUsedSpace;
+    }
+
+    public void setNonDfsUsedSpace(Long nonDfsUsedSpace) {
+        this.nonDfsUsedSpace = nonDfsUsedSpace;
+    }
+
+    public Long getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Long capacity) {
+        this.capacity = capacity;
+    }
+
+    public Long getNumBlocks() {
+        return numBlocks;
+    }
+
+    public void setNumBlocks(Long numBlocks) {
+        this.numBlocks = numBlocks;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Long getUsed() {
+        return used;
+    }
+
+    public void setUsed(Long used) {
+        this.used = used;
+    }
+
+    public Long getRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(Long remaining) {
+        this.remaining = remaining;
+    }
+
+    public Long getBlockScheduled() {
+        return blockScheduled;
+    }
+
+    public void setBlockScheduled(Long blockScheduled) {
+        this.blockScheduled = blockScheduled;
+    }
+
+    public Long getBlockPoolUsed() {
+        return blockPoolUsed;
+    }
+
+    public void setBlockPoolUsed(Long blockPoolUsed) {
+        this.blockPoolUsed = blockPoolUsed;
+    }
+
+    public Double getBlockPoolUsedPercent() {
+        return blockPoolUsedPercent;
+    }
+
+    public void setBlockPoolUsedPercent(Double blockPoolUsedPercent) {
+        this.blockPoolUsedPercent = blockPoolUsedPercent;
+    }
+
+    public Long getVolfails() {
+        return volfails;
+    }
+
+    public void setVolfails(Long volfails) {
+        this.volfails = volfails;
+    }
+}
