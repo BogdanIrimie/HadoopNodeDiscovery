bfa003eedd856c3118b8fe2ff8c8c94859fd3854
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-01-15 09:26:29 +0200

    Add unit tests.

---
+++ src/test/java/CommandExecutorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class CommandExecutorTest {
+
+    /**
+     * Test if external programs can be executed.
+     */
+    @Test
+    public void executeCommandsTest() {
+        CommandExecutor ce = new CommandExecutor();
+        String command = "echo This is it!";
+        String results = ce.execute(command);
+        assertTrue(results.equals("This is it!"));
+    }
+}
---
+++ src/test/java/XmlTOJsonConverterTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class XmlTOJsonConverterTest {
+
+    /**
+     * Test conversion from XML to JSON.
+     */
+    @Test
+    public void convertXmlToJson() {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<nodes>\n" +
+                "    <node>\n" +
+                "        <name>hdfs_dataNode_1</name>\n" +
+                "        <ip>192.168.56.120</ip>\n" +
+                "    </node>\n" +
+                "    <node>\n" +
+                "        <name>hdfs_dataNode_2</name>\n" +
+                "        <ip>192.168.56.121</ip>\n" +
+                "    </node>\n" +
+                "</nodes>";
+        String json = "{\"nodes\":{\"node\":[{\"ip\":\"192.168.56.120\",\"name\":\"hdfs_dataNode_1\"}," +
+                "{\"ip\":\"192.168.56.121\",\"name\":\"hdfs_dataNode_2\"}]}}";
+
+        XmlToJsonConverter xmlConverter = new XmlToJsonConverter();
+        String convertedJson = xmlConverter.process(xml);
+
+        assertTrue(convertedJson.equals(json));
+    }
+}
